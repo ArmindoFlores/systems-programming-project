c4478e3de65769ff7bacc6db66f0b04e13f6c88e
ArmindoFlores <francisco.rodrigues0908@gmail.com>
2021-05-04 11:52:27 +0100

    Added function signatures to KVS-lib.h and re-added client.c

--- src/KVS-lib.c
+++ src/KVS-lib.c
@@ -1,25 +1,25 @@
 #include "KVS-lib.h"
 
-int establish_connection (char * group_id, char * secret){
-	
+int establish_connection (char *group_id, char *secret)
+{
 }
 
-int put_value(char * key, char * value){
-	
+int put_value(char *key, char *value)
+{
 }
 
-int get_value(key, char ** value){
-	
+int get_value(char *key, char **value)
+{
 }
 
-int delete_value(key){
-	
+int delete_value(char *key)
+{
 }
 
-int register_callback(char * key, void (*callback_function)(char *)){
-	
+int register_callback(char *key, void (*callback_function)(char *))
+{
 }
 
-int close_connection(){
-	
+int close_connection()
+{
 }
--- src/KVS-lib.h
+++ src/KVS-lib.h
@@ -1,6 +1,11 @@
 #ifndef _KVS_LIB_
 #define _KVS_LIB_
 
-
+int establish_connection (char *group_id, char *secret);
+int put_value(char *key, char *value);
+int get_value(char *key, char **value);
+int delete_value(char *key);
+int register_callback(char *key, void (*callback_function)(char *));
+int close_connection();
 
 #endif
