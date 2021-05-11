6d6e6207527c8036820f63b9f3a4d1b0da305a48
ArmindoFlores <francisco.rodrigues0908@gmail.com>
2021-05-11 14:22:02 +0100

    Added an option to void received data

--- src/common.c
+++ src/common.c
@@ -5,11 +5,20 @@
 #include <stdio.h>
 
 
+static size_t min(size_t i, size_t j) { return i > j ? j : i; }
+
 int recvall(int socket, char *buffer, size_t n)
 {
     size_t total = 0;
     while (total < n) {
-        ssize_t recvd = recv(socket, buffer+total, (n - total), 0);
+        ssize_t recvd;
+        if (buffer != NULL)
+            recvd = recv(socket, buffer+total, (n - total), 0);
+        else {
+            char b[256];
+            recvd = recv(socket, b, min(n - total, sizeof(b)), 0);
+        }
+
         if (recvd == -1) // An error occurred
             return errno;
         else if (recvd == 0) // Client disconnected
